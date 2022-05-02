34c3e99e2e02501c36295e5a799b77b1dd20850f
HeeMang28 <22tkakd@naver.com>
2022-05-02 21:44:31 +0900

    deletecheck 수정

--- BaCode/src/main/java/kr/co/bacode/UserDeleteCheck.java
+++ BaCode/src/main/java/kr/co/bacode/UserDeleteCheck.java
@@ -1,11 +1,16 @@
 package kr.co.bacode;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kr.co.bacode.domain.userDAO;
+import kr.co.bacode.domain.userVO;
 
 /**
  * Servlet implementation class UserDeleteCheck
@@ -26,7 +31,14 @@ public class UserDeleteCheck extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		String uId = request.getParameter("uId");
+		userDAO dao = userDAO.getInstance();
+		userVO user  = dao.getUserDetail(uId);
+		System.out.println(user);
+		request.setAttribute("user", user);
+		RequestDispatcher dp = request.getRequestDispatcher("/user/userDeleteCheck.jsp");
+		dp.forward(request, response);
 	}
 
 }
