597f5e0d0ccd8f26304ae5b9f7897d36e602abc9
HeeMang28 <22tkakd@naver.com>
2022-05-20 21:26:21 +0900

    매너 온도 기능 구현 완료

--- BaCode/src/main/java/kr/co/bacode/ReviewInsert.java
+++ BaCode/src/main/java/kr/co/bacode/ReviewInsert.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import kr.co.bacode.domain.ReviewDAO;
+import kr.co.bacode.domain.userDAO;
 
 /**
  * Servlet implementation class ReviewInsert
@@ -36,8 +37,9 @@ public class ReviewInsert extends HttpServlet {
 		System.out.println("/reviewInsert 접속");
 		// DAO 생성 및 싱글턴 패턴 
 		ReviewDAO dao = ReviewDAO.getInstance();
-		String angry = request.getParameter("angry");
-		System.out.println(angry);
+		userDAO uDao = userDAO.getInstance();
+		String btnradio = request.getParameter("btnradio");
+		System.out.println(btnradio);
 		String selUid = request.getParameter("selUid");
 		String strPostNum = request.getParameter("postNum");
 		int postNum = Integer.parseInt(strPostNum);
@@ -46,6 +48,12 @@ public class ReviewInsert extends HttpServlet {
 		String content = request.getParameter("content");
 		// ReviewDAO에서 게시판 등록 메서드 실행
 		dao.insertReview(postNum, uId, title, content);
+		
+		if (btnradio.equals("angry")) {
+		 uDao.downHeart(selUid);
+		} else if(btnradio.equals("heartEyes")) {
+			uDao.upHeart(selUid);
+		} 
 		response.sendRedirect("http://localhost:52525/BaCode/getReviewList");
 	}
 
